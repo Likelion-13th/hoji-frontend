7d3bb88f659bad62d1148315f68f10f710aeed22
asfslaf <jiho2305@gmail.com>
2025-03-29 17:32:26 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Footer from "./component/Footer";
 import Home from "./pages/Home/Home";
-import Mypage from "./pages/Mypage/Mypage"
-import Diffuser from "./pages/ProductPage/Diffuser"
-import Perfume from "./pages/ProductPage/Perfume"
-import New from "./pages/ProductPage/New"
+import Mypage from "./pages/Mypage/Mypage";
+import Diffuser from "./pages/ProductPage/Diffuser";
+import Perfume from "./pages/ProductPage/Perfume";
+import New from "./pages/ProductPage/New";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/perfume" element ={<Perfume />} />
         <Route path="/new" element ={<New />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+        <div className="footer-section">
+        <div className="footer-title">
+            LIKELION PROCESS
+        </div>
+        <div className="footer-subtitle">
+            멋쟁이사자처럼은 창업 동아리이므로 실제 상품을 판매하지 않습니다.
+        </div>
+        </div>
+        <div className='footer-section'>
+        <div className='info-text-row'>
+        <div className="info-text-wrapper">
+            <div className="info-text-1">상호명</div>
+            <div className="info-text-2">멋쟁이사자처럼</div>
+        </div>
+        <div className="info-text-wrapper">
+            <div className="info-text-1">대표</div>
+            <div className="info-text-2">이지호</div>
+        </div>
+        <div className="info-text-wrapper">
+            <div className="info-text-1">사업자등록번호</div>
+            <div className="info-text-2">122-12-124124</div>
+        </div>
+        <div className="info-text-wrapper">
+            <div className="info-text-1">주소</div>
+            <div className="info-text-2">우리집</div>
+        </div>
+        </div>
+        <div className='info-text-row'>
+        <div className="info-text-wrapper">
+            <div className="info-text-1">전화번호</div>
+            <div className="info-text-2">1214-1256</div>
+        </div>
+        <div className="info-text-wrapper">
+            <div className="info-text-1">개인정보보호책임자</div>
+            <div className="info-text-2">이지호</div>
+        </div>
+        <div className="info-text-wrapper">
+            <div className="info-text-1">계좌</div>
+            <div className="info-text-2">122-12-124124</div>
+        </div>
+        </div>
+        </div>
+        </div>
+    );
+}
+
+export default Footer;
