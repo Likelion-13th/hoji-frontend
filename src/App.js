e467ba48ae6d17bcde22ba01061cfbd17fdcc5f9
asfslaf <jiho2305@gmail.com>
2025-04-09 19:49:05 +0900

    toolbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Home from "./pages/Home/Home";
+import ToolBar from './pages/Home/ToolBar';
 import Mypage from "./pages/Mypage/Mypage";
 import Diffuser from "./pages/ProductPage/Diffuser";
 import Perfume from "./pages/ProductPage/Perfume";
@@ -14,12 +15,13 @@ function App() {
   return (
     <Router>
       <Header />
+      <ToolBar />
       <Routes>
-        <Route path="/" element ={<Home />} />
-        <Route path="/mypage" element ={<Mypage />} />
-        <Route path="/diffuser" element ={<Diffuser />} />
-        <Route path="/perfume" element ={<Perfume />} />
-        <Route path="/new" element ={<New />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/diffuser" element={<Diffuser />} />
+        <Route path="/perfume" element={<Perfume />} />
+        <Route path="/new" element={<New />} />
       </Routes>
       <Footer />
     </Router>
