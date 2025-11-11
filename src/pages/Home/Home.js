33eb53e75e2ff6ad6f1ecd92d634293ad4549efe
asfslaf <jiho2305@gmail.com>
2025-11-11 19:13:24 +0900

    Netlify 배포 오류: Home.js에서 cookie 사용안함

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,7 @@ import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 
 const Home = ({onLoginChange}) => {
-    const [cookies, setCookie] = useCookies(['accessToken']);
+    const [, setCookie] = useCookies(['accessToken']);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -20,8 +20,8 @@ const Home = ({onLoginChange}) => {
                 maxAge: 60 * 60 * 24 * 7,
             });
             
-            onLoginChange(true);
             navigate("/", { replace: true });
+            onLoginChange(true);
         }
     }, [setCookie, navigate, onLoginChange]);
 
